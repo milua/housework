75244f5b96c171bc3d97f077735ee881115b7a37
Isabella Tran <tran.isabella93@gmail.com>
2021-02-12 20:32:27 +0100

    updated README

--- README.md
+++ README.md
@@ -7,3 +7,5 @@ It is also my personal flutter playground
 - Install Flutter
 - Have an IDE Setup and a device (I personally am using an Android SDK 28  here)
 
+## Prototype
+You can find the click prototype here: https://www.figma.com/proto/xAHHCRzhPCxUUKDL0w745b/Houswork-Game?node-id=0%3A1&scaling=scale-down
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:housework/bottom-navigation.dart';
 
 void main() => runApp(MyApp());
 
@@ -10,80 +11,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: _title,
-      home: NavigationWidget(),
-    );
-  }
-}
-
-/// This is the stateful widget that the main application instantiates.
-class NavigationWidget extends StatefulWidget {
-  NavigationWidget({Key key}) : super(key: key);
-
-  @override
-  _NavigationWidgetState createState() => _NavigationWidgetState();
-}
-
-/// This is the private State class that goes with MyStatefulWidget.
-class _NavigationWidgetState extends State<NavigationWidget> {
-  int _selectedIndex = 0;
-  static const TextStyle optionStyle =
-  TextStyle(fontSize: 30, fontWeight: FontWeight.bold, color: Colors.black54);
-  static const List<Widget> _widgetOptions = <Widget>[
-    Text(
-      'My Account',
-      style: optionStyle,
-    ),
-    Text(
-      'Task List',
-      style: optionStyle,
-    ),
-    Text(
-      'Reward List',
-      style: optionStyle,
-    ),
-    Text(
-      'Settings',
-      style: optionStyle,
-    ),
-  ];
-
-  void _onItemTapped(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: _widgetOptions.elementAt(_selectedIndex),
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.account_circle_outlined),
-            label: 'Account',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.check_box_outlined),
-            label: 'Tasks',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.redeem_outlined),
-            label: 'Rewards',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.settings_outlined),
-            label: 'Settings',
-          ),
-        ],
-        currentIndex: _selectedIndex,
-        selectedItemColor: Colors.green[500],
-        unselectedItemColor: Colors.black54,
-        showUnselectedLabels: true,
-        onTap: _onItemTapped,
-      ),
+      home: BottomNavigationWidget(),
     );
   }
 }
